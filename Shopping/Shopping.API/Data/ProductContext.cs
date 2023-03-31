665eabb602017313b98e9d7f7dcddac308404dda
Nick Petropoulos <nick.petropoulos@gmail.com>
2023-04-01 01:03:35 +0300

    deploy shoppingapi pipeline CHANGED

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -36,7 +36,7 @@ namespace Shopping.API.Data
                 {
                     new Product()
                     {
-                        Name = "IPhone UV",
+                        Name = "IPhone UVA-DDD",
                         Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                         ImageFile = "product-1.png",
                         Price = 1950.00M,
